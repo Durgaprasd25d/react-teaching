487dcfef1de6b0ed4669db8036e6f1be5577f123
Durgaprasad Dalai <durgaprasaddalai10@gmail.com>
2025-04-11 16:29:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Lucky from "./components/Lucky";
+const App = () => {
+  return (
+    <div>
+      <Lucky name="Lucky" />
+      <Lucky name="Roja" />
+      <Lucky name="Naz" />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Lucky.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Fa1 } from "react-icons/fa6";
+const Lucky = ({name}) => {
+  return <h2>This is me {name}</h2>;
+};
+export default Lucky;
